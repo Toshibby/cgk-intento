43bcbf9753f70b6808f101a21c1df39c4385b4d0
Ciro Machicado <cirogkmv@gmail.com>
2025-09-13 00:58:04 -0500

    eureka

---
+++ cgk-registry-server/src/test/java/com/upeu/cgk_registry_server/CgkRegistryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upeu.cgk_registry_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgkRegistryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
