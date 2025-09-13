24c0d43564bd996d3813392c070a8e531db60d08
Ciro Machicado <cirogkmv@gmail.com>
2025-09-13 01:05:52 -0500

    agregando gateway

---
+++ cgk-gateway/src/main/java/com/upeu/cgk_gateway/CgkGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.upeu.cgk_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgkGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgkGatewayApplication.class, args);
+	}
+
+}
---
+++ cgk-gateway/src/test/java/com/upeu/cgk_gateway/CgkGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upeu.cgk_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgkGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
